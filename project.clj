89a85702400e836d0e044303587ef2ffc4f0a593
Joe Wass <joe@afandian.com>
2016-09-13 16:56:07 +0100

    License.

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
 (defproject event-data-newsfeed-agent "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Crossref Event Data Newsfeed Agent"
+  :url "http://eventdata.crossref.org"
+  :license {:name "The MIT License (MIT)"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.crossref/event-data-agent-framework "0.1.0"]
                  [com.rometools/rome "1.6.1"]
                  [org.clojure/java.jdbc "0.4.2"]
-                [mysql-java "5.1.21"] 
+                 [mysql-java "5.1.21"] 
                  [korma "0.4.0"]]
   :main ^:skip-aot event-data-newsfeed-agent.core
   :target-path "target/%s"
