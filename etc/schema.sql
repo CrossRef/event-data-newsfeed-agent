86eb04cfef3ecc3f7dc1704abef4c91ac28732f3
Joe Wass <jwass@crossref.org>
2017-04-05 18:26:39 +0100

    Remove unused SQL schema.

--- etc/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE seen_blog_url (
-  id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  blog_url VARCHAR(1024),
-  feed_url VARCHAR(1024),
-  seen DATETIME
-) ENGINE=InnoDB CHARACTER SET=utf8mb4;
-
--- Typical URLs are ~50 characters.
-CREATE INDEX blog_url_name ON seen_blog_url(blog_url(128));
